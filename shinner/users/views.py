03eca3c9a4e5f40c15709ebf90fa064f37c00158
AAlfaro29 <86653594+AAlfaro29@users.noreply.github.com>
2021-10-29 14:57:23 +1000

    Last changes

--- shinner/users/views.py
+++ shinner/users/views.py
@@ -1,41 +1,58 @@
+from django.shortcuts import render
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import CustomUser
-from .serializers import CustomUserSerializer
+from .serializers import CustomUserSerializer, CustomUserSerializer, CreateUserSerializer#, #UserDetailSerializer
+from django.http import Http404
 
-# /users
 class CustomUserList(APIView):
-    # GET method - retrieve all users
     def get(self, request):
         users = CustomUser.objects.all()
         serializer = CustomUserSerializer(users, many=True)
+        #serializer = CreateUserSerializer(data=request.data)
         return Response(serializer.data)
-        
-    # POST method - create a new user
+
     def post(self, request):
-        serializer = CustomUserSerializer(data=request.data)
+        #serializer = CustomUserSerializer(data=request.data)
+        serializer = CreateUserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# /users/<pk>
 class CustomUserDetail(APIView):
-   
-    # helper method for getting a user and raising a 404 if that user does not exist
     def get_object(self, pk):
-        # try getting the user with the specified pk
         try:
             return CustomUser.objects.get(pk=pk)
-        # catch the exception that we'll get if the user with that pk doesn't exist
         except CustomUser.DoesNotExist:
-            # raise an Http404 exception so that Django knows to show a 404
             raise Http404
 
-    # GET a single user's detail
     def get(self, request, pk):
         user = self.get_object(pk)
-       	serializer = CustomUserSerializer(user)
+        serializer = CustomUserSerializer(user)
         return Response(serializer.data)
+    
+    def put(self, request, pk):
+        user = self.get_object(pk)
+        data = request.data
+        serializer = CustomUserSerializer(
+        #UserDetailSerializer(
+            instance=user,
+            data=data,
+            partial=True
+        ) 
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                )
+        return Response(
+        serializer.errors,
+        status=status.HTTP_400_BAD_REQUEST
+        )
+    def delete(self, request, pk, format=None):
+        user = self.get_object(pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
